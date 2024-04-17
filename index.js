061ba0e6413a2497c75102f90f818e902aec2997
josephmb830 <josephmoran@duck.com>
2024-04-16 20:35:17 -0500

    retorno de velocidad

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function calcularVelocidad(distancia, tiempo) {
-    // const distancia = 10
-    // const tiempo = 2
+const calcularVelocidad = (distancia, tiempo) => {
     const velocidad = distancia / tiempo;
     console.log("La velocidad del móvil es " + velocidad);
+        return velocidad
     }
     
+    
 
-    calcularVelocidad(10,2);
-    calcularVelocidad(100,2);
-    calcularVelocidad(75,3);
+    const velocidad1 = calcularVelocidad(10,2);
+    console.log("La velocidad de Juana es " + velocidad1);
+    const velocidad2 = calcularVelocidad(100,2);
+    console.log("La velocidad de Mateo es " + velocidad2);
+    const velocidad3 = calcularVelocidad(75,3);
+    console.log("La velocidad de Ana es " + velocidad3);
     
