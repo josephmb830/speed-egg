b91b296735b95c491052b34c6fd06be6fa63dd8b
josephmb830 <josephmoran@duck.com>
2024-04-16 20:27:07 -0500

    velocidad dinamica

---
+++ index.js
@@ -0,0 +1,12 @@
+function calcularVelocidad(distancia, tiempo) {
+    // const distancia = 10
+    // const tiempo = 2
+    const velocidad = distancia / tiempo;
+    console.log("La velocidad del móvil es " + velocidad);
+    }
+    
+
+    calcularVelocidad(10,2);
+    calcularVelocidad(100,2);
+    calcularVelocidad(75,3);
+    
