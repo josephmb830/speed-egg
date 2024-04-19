fb6dcde978902b8e5b1f32b93dce6c404e027cae
josephmb830 <josephmoran@duck.com>
2024-04-18 21:05:47 -0500

    alertas agregadas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const calcularVelocidad = (distancia, tiempo) => {
     const velocidad = distancia / tiempo;
     console.log("La velocidad del móvil es " + velocidad);
+    alert("La velocidad del móvil es " + velocidad);
         return velocidad
     }
     
@@ -8,8 +9,10 @@ const calcularVelocidad = (distancia, tiempo) => {
 
     const velocidad1 = calcularVelocidad(10,2);
     console.log("La velocidad de Juana es " + velocidad1);
+    alert("La velocidad de Juana es " + velocidad1);
     const velocidad2 = calcularVelocidad(100,2);
     console.log("La velocidad de Mateo es " + velocidad2);
+    alert("La velocidad de Mateo es " + velocidad2);
     const velocidad3 = calcularVelocidad(75,3);
     console.log("La velocidad de Ana es " + velocidad3);
-    
+    alert("La velocidad de Ana es " + velocidad3);
